07e8ba453dcf8a435efd5bf8bbd54485a830b579
Matthew Brian Cohen <cohen@matthewbriancohen.com>
2025-05-22 11:12:47 -0700

    fixed broken pihole cert renewal process

--- HomeLab/roles/pihole/files/pihole-pem.sh
+++ HomeLab/roles/pihole/files/pihole-pem.sh
@@ -1,15 +1,39 @@
 #!/usr/bin/bash
 # Creates a pem file with the fullchain and key, as pihole annoyingly wants
-CERTS=("homelab_pihole_full.pem")
-CERT_DIR="/etc/letsencrypt/live/$(hostname)"
+CERT="homelab_pihole_full.pem"
+CERT_DIR="/etc/letsencrypt/live/$(hostname).localdomain"
 PIHOLE_CERT_DIR="/etc/pihole"
-cat "$(readlink -f "$CERT_DIR"/privkey.pem)" "$(readlink -f "$CERT_DIR"/fullchain.pem)" >"$CERT_DIR"/homelab_pihole_full.pem
-for cert in "${CERTS[@]}"; do
-    if [[ -f $PIHOLE_CERT_DIR/$cert ]]; then
-        echo "hardlink $PIHOLE_CERT_DIR/$cert exists, skipping"
-    else
-        ln "$CERT_DIR"/"$cert" "$PIHOLE_CERT_DIR"/"$cert"
-        chown pihole:pihole "$PIHOLE_CERT_DIR/$cert"
-        chmod 0600 "$PIHOLE_CERT_DIR/$cert"
-    fi
-done
+
+# delete old pihole cert
+if [[ -f "$PIHOLE_CERT_DIR/$CERT" ]]; then
+    rm "$PIHOLE_CERT_DIR/$CERT"
+fi
+if [[ -f "$CERT_DIR/$CERT" ]]; then
+    rm "$CERT_DIR/$CERT"
+fi
+# create the full chain cert
+cat "$CERT_DIR/privkey.pem" "$CERT_DIR/fullchain.pem" >"$CERT_DIR/$CERT"
+cp "$CERT_DIR/$CERT" "$PIHOLE_CERT_DIR/$CERT"
+chown pihole:pihole "$PIHOLE_CERT_DIR/$CERT"
+chmod 0660 "$PIHOLE_CERT_DIR/$CERT"
+#!/usr/bin/bash
+# Creates a pem file with the fullchain and key, as pihole annoyingly wants
+CERT="homelab_pihole_full.pem"
+CERT_DIR="/etc/letsencrypt/live/$(hostname).localdomain"
+PIHOLE_CERT_DIR="/etc/pihole"
+
+# delete old pihole cert
+if [[ -f "$PIHOLE_CERT_DIR/$CERT" ]]; then
+    rm "$PIHOLE_CERT_DIR/$CERT"
+fi
+if [[ -f "$CERT_DIR/$CERT" ]]; then
+    rm "$CERT_DIR/$CERT"
+fi
+# create the full chain cert
+cat "$CERT_DIR/privkey.pem" "$CERT_DIR/fullchain.pem" >"$CERT_DIR/$CERT"
+cp "$CERT_DIR/$CERT" "$PIHOLE_CERT_DIR/$CERT"
+chown pihole:pihole "$PIHOLE_CERT_DIR/$CERT"
+chmod 0660 "$PIHOLE_CERT_DIR/$CERT"
+
+#restart pihole service
+systemctl restart pihole-FTL.service
