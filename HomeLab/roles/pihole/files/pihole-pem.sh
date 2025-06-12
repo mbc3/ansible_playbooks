c024c7d1d985b60813a60585ce00bb12847c88e5
Matthew Brian Cohen <cohen@matthewbriancohen.com>
2025-06-12 16:56:08 -0700

    fixed pihole cert renewal script, git task

--- HomeLab/roles/pihole/files/pihole-pem.sh
+++ HomeLab/roles/pihole/files/pihole-pem.sh
@@ -6,28 +6,10 @@ PIHOLE_CERT_DIR="/etc/pihole"
 
 # delete old pihole cert
 if [[ -f "$PIHOLE_CERT_DIR/$CERT" ]]; then
-    rm "$PIHOLE_CERT_DIR/$CERT"
+  rm "$PIHOLE_CERT_DIR/$CERT"
 fi
 if [[ -f "$CERT_DIR/$CERT" ]]; then
-    rm "$CERT_DIR/$CERT"
-fi
-# create the full chain cert
-cat "$CERT_DIR/privkey.pem" "$CERT_DIR/fullchain.pem" >"$CERT_DIR/$CERT"
-cp "$CERT_DIR/$CERT" "$PIHOLE_CERT_DIR/$CERT"
-chown pihole:pihole "$PIHOLE_CERT_DIR/$CERT"
-chmod 0660 "$PIHOLE_CERT_DIR/$CERT"
-#!/usr/bin/bash
-# Creates a pem file with the fullchain and key, as pihole annoyingly wants
-CERT="homelab_pihole_full.pem"
-CERT_DIR="/etc/letsencrypt/live/$(hostname).localdomain"
-PIHOLE_CERT_DIR="/etc/pihole"
-
-# delete old pihole cert
-if [[ -f "$PIHOLE_CERT_DIR/$CERT" ]]; then
-    rm "$PIHOLE_CERT_DIR/$CERT"
-fi
-if [[ -f "$CERT_DIR/$CERT" ]]; then
-    rm "$CERT_DIR/$CERT"
+  rm "$CERT_DIR/$CERT"
 fi
 # create the full chain cert
 cat "$CERT_DIR/privkey.pem" "$CERT_DIR/fullchain.pem" >"$CERT_DIR/$CERT"
