b2326c102ed2828dce2e57692042e055f9cf00d2
Matthew Brian Cohen <cohen@matthewbriancohen.com>
2025-08-08 15:47:40 -0700

    fixed wazuh.sh to add new certs upon renewal

--- HomeLab/roles/wazuh_server/files/wazuh.sh
+++ HomeLab/roles/wazuh_server/files/wazuh.sh
@@ -1,3 +1,14 @@
 #!/usr/bin/bash
+# Copy fullchain.pem and pivkey.pem to wazuh for tls
+HOSTNAME=$(hostname | cut -f 1 -d ".")
+DOMAIN="localdomain"
+CERT_DIR="/etc/letsencrypt/live/$HOSTNAME.$DOMAIN"
+WAZUH_CERT_DIR="/etc/wazuh-dashboard/certs"
+CERTS=("fullchain.pem" "privkey.pem")
 
+for cert in "${CERTS[@]}"; do
+  cp "$CERT_DIR/$cert" "$WAZUH_CERT_DIR/$cert"
+  chown wazuh-dashboard:wazuh-dashboard "$WAZUH_CERT_DIR/$cert"
+  chmod 0400 "$WAZUH_CERT_DIR/$cert"
+done
 /bin/systemctl restart wazuh-dashboard
